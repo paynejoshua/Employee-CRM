f1a09d5a56efadf57c5749253e0e2941d954f1ef
Josh Payne <paynejoshua1@gmail.com>
2020-09-17 11:33:53 -0700

    update employees first name is completed

--- app.js
+++ app.js
@@ -404,16 +404,6 @@ updateEmployee = () => {
                       name: "fullName",
                       type: "input",
                       message: "What is this employees new full name?"
-                      //   validate: (input) => {
-                      //     return new Promise((resolve, reject)=> {
-                      //       const words = input.split(' ')
-                      //       if (words.length !== 2) {
-                      //          reject('Please put space between first and last name');
-                      //       }
-                      //       resolve(true);
-                      //     })
-  
-                      //  }
                     }
                   ])
                   .then(function (answer) {
@@ -436,51 +426,33 @@ updateEmployee = () => {
                   break;
                 default:
                   break;
-  
+              case "First Name":
+                inquirer
+                .prompt([
+                  {
+                    name: "firstName",
+                    type: "input",
+                    message: "What is this employees new first name?"
+                  }
+                ])
+                .then(function (answer){
+                  db.query("UPDATE employee SET ? WHERE ?",
+                  [
+                    {
+                      first_name: answer.firstName
+                    },
+                    {
+                      id: employeeID
+                    }
+                    
+                  ],
+                  function (err) {
+                    if(err) throw err;
+                  })
+                  viewEmployee();
+                })
+                break;
           }
-          // console.log(answer.information)
-          // switch (answer.information) {
-          //   case "Full Name":
-          //     console.log("inside case Full Name");
-          //     answer.information = '';
-          //     break;
-          //   // inquirer
-          //   //     .prompt([
-          //   //       {
-          //   //         name: "fullName",
-          //   //         type: "input",
-          //   //         message: "What is this employees new full name?"
-          //   //         //   validate: (input) => {
-          //   //         //     return new Promise((resolve, reject)=> {
-          //   //         //       const words = input.split(' ')
-          //   //         //       if (words.length !== 2) {
-          //   //         //          reject('Please put space between first and last name');
-          //   //         //       }
-          //   //         //       resolve(true);
-          //   //         //     })
-
-          //   //         //  }
-          //   //       }
-          //   //     ])
-          //   //     .then(function (answer) {
-          //   //       const words = answer.fullName.split(' ');
-          //   //       db.query("UPDATE employee SET ? WHERE ?",
-          //   //         [
-          //   //           {
-          //   //             first_name: words[0],
-          //   //             last_name: words[1]
-          //   //           },
-          //   //           {
-          //   //             id: employeeID
-          //   //           }
-          //   //         ],
-          //   //         function (err) {
-          //   //           if (err) throw err;
-          //   //         })
-          //   //     })
-          //   //     break;
-          //   //   default:
-          //   //     break;
           }
         }
       })
